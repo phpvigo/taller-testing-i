f6dbcdac048254286ea0a47efa3f68b236c93e91
Santiago Rodríguez <rocasan@gmail.com>
2017-05-25 14:17:31 +0000

    rocasan: minimal compliant with Kata rules, but not directly runnable in cli

--- FizzBuzz/Solutions/rocasan/src/FizzBuzz.php
+++ FizzBuzz/Solutions/rocasan/src/FizzBuzz.php
@@ -8,4 +8,40 @@ class FizzBuzz {
 		return range(1,100);
 	}
 
+	/**
+	 * Make substitutions in the array $list as follows:
+	 * - Si un número divisible por 3 se reemplazará por 'Fizz'
+	 * - Si un número es divisible por 5 se reemplazará por 'Buzz'
+	 * - Un número divisible por 3 y por 5 se reemplazará por 'FizzBuzz'
+	 * 
+	 * @param array $list
+	 * @return array
+	 */
+	public function substituteList($list) {
+		$substitutions = $list;
+		foreach($substitutions as $i => $elem) {
+			$substitutions[$i] = $this->processElement($elem);
+		}
+		return $substitutions;
+	}
+	/**
+	 * Substitute the element, if needed with the kata rules:
+	 * - Si un número divisible por 3 se reemplazará por 'Fizz'
+	 * - Si un número es divisible por 5 se reemplazará por 'Buzz'
+	 * - Un número divisible por 3 y por 5 se reemplazará por 'FizzBuzz'
+	 * 
+	 * @param int $elem
+	 * @return int
+	 */
+	protected function processElement($elem) {
+		$substitution = '';
+		if($elem % 3 == 0) {
+			$substitution .= 'Fizz';
+		}
+		if($elem % 5 == 0) {
+			$substitution .= 'Buzz';
+		}
+		return empty($substitution) ? $elem : $substitution;
+	}
+
 }
--- FizzBuzz/Solutions/rocasan/test/FizzBuzzTest.php
+++ FizzBuzz/Solutions/rocasan/test/FizzBuzzTest.php
@@ -8,15 +8,32 @@ use PHPUnit\Framework\TestCase;
 
 class FizzBuzzTest extends TestCase
 {
+	
+	protected $fizzBuzz;
+	
+	public function setUp() {
+		$this->fizzBuzz = new FizzBuzz();
+	}
+	
 	/**
 	 * "En la kata FizzBuzz se debe producir un listado de números"
 	 */
     public function testMustProduceAListOfNumbers() {
-		$fizzBuzz = new FizzBuzz();
-		$list = $fizzBuzz->produceList();
+		$list = $this->fizzBuzz->produceList();
 		$this->assertTrue(is_array($list),"FizzBuzz doesn't produce a list");
 		foreach($list as $elem) {
 			$this->assertTrue(is_numeric($elem),"'$elem' should be a number");
 		}
 	}
+	
+	/**
+	 * Kata: "algunos de los cuales serán sustituidos por cadenas de acuerdo con las siguientes condiciones"
+	 */
+	public function testSubstitutions() {
+		$this->assertEquals(
+			[1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz","Buzz",11,"Fizz",13,14,"FizzBuzz",16,17,"Fizz",19,"Buzz"], 
+			$this->fizzBuzz->substituteList([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
+		);
+	}
+	
 }
