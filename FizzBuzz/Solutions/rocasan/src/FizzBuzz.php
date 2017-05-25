aac1f1146cf26deae76f00c0c8674dcdaa18e56f
Santiago Rodríguez <rocasan@gmail.com>
2017-05-25 12:47:57 +0000

    rocasan: added first TDD cicle

--- FizzBuzz/Solutions/rocasan/src/FizzBuzz.php
+++ FizzBuzz/Solutions/rocasan/src/FizzBuzz.php
@@ -4,5 +4,8 @@ namespace FizzBuzz;
 
 class FizzBuzz {
 
+	public function produceList() {
+		return range(1,100);
+	}
 
 }
--- FizzBuzz/Solutions/rocasan/test/FizzBuzzTest.php
+++ FizzBuzz/Solutions/rocasan/test/FizzBuzzTest.php
@@ -2,9 +2,21 @@
 
 namespace FizzBuzz\Test;
 
+use \FizzBuzz\FizzBuzz;
+
 use PHPUnit\Framework\TestCase;
 
 class FizzBuzzTest extends TestCase
 {
-    
+	/**
+	 * "En la kata FizzBuzz se debe producir un listado de números"
+	 */
+    public function testMustProduceAListOfNumbers() {
+		$fizzBuzz = new FizzBuzz();
+		$list = $fizzBuzz->produceList();
+		$this->assertTrue(is_array($list),"FizzBuzz doesn't produce a list");
+		foreach($list as $elem) {
+			$this->assertTrue(is_numeric($elem),"'$elem' should be a number");
+		}
+	}
 }
