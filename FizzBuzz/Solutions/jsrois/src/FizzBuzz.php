3679163359468c0cfcdbf853359c73b0df562dcf
jsrois <javier.sanchez.rois@gmail.com>
2017-05-26 12:41:03 +0200

    [GREEN] passing the first test is always easy.

---
+++ FizzBuzz/Solutions/jsrois/src/FizzBuzz.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jsrois
+ * Date: 26/5/17
+ * Time: 12:37
+ */
+
+namespace FizzBuzz;
+
+
+class FizzBuzz
+{
+    public function of($number) {
+        return "1";
+    }
+}
