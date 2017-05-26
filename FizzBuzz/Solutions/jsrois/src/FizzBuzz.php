ff8daaa7de41fdcd87e8fc936f8c3973f9284235
jsrois <javier.sanchez.rois@gmail.com>
2017-05-26 13:18:13 +0200

    [REFACTOR: EXTRACT METHOD] isFizz

--- FizzBuzz/Solutions/jsrois/src/FizzBuzz.php
+++ FizzBuzz/Solutions/jsrois/src/FizzBuzz.php
@@ -11,9 +11,17 @@ namespace FizzBuzz;
 class FizzBuzz
 {
     public function of($number) {
-        if ($number %3 == 0) {
+        if ($this->isFizz($number)) {
             return "Fizz";
         }
         return (string) $number;
+    }
+
+    /**
+     * a number is fizz if it's multiple of three
+     */
+    private function isFizz($number)
+    {
+        return $number % 3 == 0;
     }
 }
