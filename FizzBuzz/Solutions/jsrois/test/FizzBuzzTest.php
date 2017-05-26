f0de4393d32a7c486ef31e565bea7ce416fe8f7d
jsrois <javier.sanchez.rois@gmail.com>
2017-05-26 12:36:22 +0200

    [RED] Initial failing test. This test is important because it says how I want the code to look like

--- FizzBuzz/Solutions/jsrois/test/FizzBuzzTest.php
+++ FizzBuzz/Solutions/jsrois/test/FizzBuzzTest.php
@@ -4,7 +4,12 @@ namespace FizzBuzz\Test;
 
 use PHPUnit\Framework\TestCase;
 
+use FizzBuzz\FizzBuzz;
+
 class FizzBuzzTest extends TestCase
 {
-    
+    function test_the_answer_for_one_is_one() {
+        $fizzBuzz = new FizzBuzz();
+        $this->assertEquals("1", $fizzBuzz->of(1));
+    }
 }
