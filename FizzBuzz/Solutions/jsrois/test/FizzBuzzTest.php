1d4aa44363b38fb9bf1feefb6e83413217eb8f2c
jsrois <javier.sanchez.rois@gmail.com>
2017-05-26 13:06:44 +0200

    [REFACTOR] The test code is also code\!

--- FizzBuzz/Solutions/jsrois/test/FizzBuzzTest.php
+++ FizzBuzz/Solutions/jsrois/test/FizzBuzzTest.php
@@ -8,13 +8,9 @@ use FizzBuzz\FizzBuzz;
 
 class FizzBuzzTest extends TestCase
 {
-    function test_the_answer_for_one_is_one() {
+    function test_the_default_answer_is_the_number() {
         $fizzBuzz = new FizzBuzz();
         $this->assertEquals("1", $fizzBuzz->of(1));
-    }
-
-    function test_the_answer_for_two_is_two() {
-        $fizzBuzz = new FizzBuzz();
         $this->assertEquals("2", $fizzBuzz->of(2));
     }
 }
