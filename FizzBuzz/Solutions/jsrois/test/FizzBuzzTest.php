92318b7fe4066c786fb5bfe70672f316d3187f77
jsrois <javier.sanchez.rois@gmail.com>
2017-05-26 13:13:34 +0200

    [REFACTOR] DRY test code

--- FizzBuzz/Solutions/jsrois/test/FizzBuzzTest.php
+++ FizzBuzz/Solutions/jsrois/test/FizzBuzzTest.php
@@ -8,16 +8,21 @@ use FizzBuzz\FizzBuzz;
 
 class FizzBuzzTest extends TestCase
 {
+    private $fizzBuzz;
+
+    function setUp() {
+        $this->fizzBuzz = $fizzBuzz = new FizzBuzz();
+    }
+
     function test_the_default_answer_is_the_number() {
-        $fizzBuzz = new FizzBuzz();
-        $this->assertEquals("1", $fizzBuzz->of(1));
-        $this->assertEquals("2", $fizzBuzz->of(2));
+
+        $this->assertEquals("1", $this->fizzBuzz->of(1));
+        $this->assertEquals("2", $this->fizzBuzz->of(2));
     }
 
     public function test_the_answer_for_three_is_fizz()
     {
-        $fizzBuzz = new FizzBuzz();
-        $this->assertEquals("Fizz", $fizzBuzz->of(3));
+        $this->assertEquals("Fizz", $this->fizzBuzz->of(3));
     }
 
 
