fdf6b2b762c4edf27c6cfea68584544fe8da2d05
Marcos Redondo <kusflo@gmail.com>
2017-05-28 12:15:11 +0200

    [RED] The Fizz Case. The second test fail.

--- FizzBuzz/Solutions/kusflo/test/FizzBuzzTest.php
+++ FizzBuzz/Solutions/kusflo/test/FizzBuzzTest.php
@@ -12,11 +12,18 @@ use PHPUnit\Framework\TestCase;
 class FizzBuzzTest extends TestCase
 {
 
-    public function testValueOfOneIsOne()
+    public function testValueIsNumber()
     {
         $fizzbuzz = new FizzBuzz();
         $this->assertEquals($fizzbuzz->getValueOf(1), 1);
+        $this->assertEquals($fizzbuzz->getValueOf(2), 2);
+        $this->assertEquals($fizzbuzz->getValueOf(4), 4);
+    }
 
+    public function testValueIsFizz()
+    {
+        $fizzbuzz = new FizzBuzz();
+        $this->assertEquals($fizzbuzz->getValueOf(3), 'Fizz');
     }
 
 }
